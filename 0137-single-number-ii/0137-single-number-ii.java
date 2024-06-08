b04d2a9500ee4e9867da24a90ba394380c892c45
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-06-08 20:44:08 +0900

    Time: 7 ms (9.24%), Space: 45.5 MB (30.04%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,5 +1,22 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i: nums) {
+            //Integer num = new Integer(i);
+            if(map.containsKey(i) ==true) {
+                map.put(i, map.get(i) + 1);
+                int count = map.get(i);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        System.out.println(map.size());
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getValue() == 1) {
+                return entry.getKey().intValue();
+            }
+        }
+        /*
         Integer result = -1;
         List<Integer> list =Arrays.stream(nums).boxed().collect(Collectors.toList());
         for(Integer i: list) {
@@ -9,6 +26,7 @@ class Solution {
             }
         }
         
-        return result.intValue();
+        return result.intValue();*/
+        return 0;
     }
 }
