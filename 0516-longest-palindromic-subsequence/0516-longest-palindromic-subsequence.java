dd7ab0809c7a466ab2d22231372a42245dec2028
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-06-11 15:41:49 +0900

    Time: 35 ms (72.69%), Space: 55.3 MB (25.79%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -13,9 +13,11 @@ class Solution {
     
     public int longestPalindromeSubseq(String s) { 
         //return lps(s.toCharArray(), 0, s.length()-1); time limit
+        
         int len = s.length();
         int [][] arr = new int [len][len];
-
+    
+        /*
         for(int i = len -1; i >= 0; i--) {
             arr[i][i] = 1;
             for(int j = i +1 ; j< len; j++) {
@@ -24,7 +26,19 @@ class Solution {
                 else
                     arr[i][j] = Math.max(arr[i][j-1], arr[i+1][j]);
             } 
+        }*/
+        
+        for(int i = 0; i < len; i++) {
+            arr[i][i] = 1;
+            for(int j= i-1; j >= 0; j--) {
+                if(s.charAt(i) == s.charAt(j))
+                    arr[j][i] = arr[j+1][i-1] + 2;
+                else
+                    arr[j][i] = Math.max(arr[j][i-1], arr[j+1][i]);
+            }
         }
+        
+        
         return arr[0][len-1];
     }
 }
