f318bb63612bf24c43362cc9303ef0cbd35d8b23
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-06-14 19:04:10 +0900

    Time: 9 ms (93.61%), Space: 57.1 MB (21.76%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,20 +10,68 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null) 
+         if(head == null || head.next == null) 
             return head;
+        
+        // step 1. cut the list to two halves
+        ListNode prev = null, slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+          prev = slow;
+          slow = slow.next;
+          fast = fast.next.next;
+        }
+
+        prev.next = null;
+
+        // step 2. sort each half
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+
+        // step 3. merge l1 and l2
+        return merge(l1, l2);
+      }
+
+      ListNode merge(ListNode l1, ListNode l2) {
+        ListNode l = new ListNode(0), p = l;
+
+        while (l1 != null && l2 != null) {
+          if (l1.val < l2.val) {
+            p.next = l1;
+            l1 = l1.next;
+          } else {
+            p.next = l2;
+            l2 = l2.next;
+          }
+          p = p.next;
+        }
+
+        if (l1 != null)
+          p.next = l1;
+
+        if (l2 != null)
+          p.next = l2;
+
+        return l.next;
+      }
+    /*
+     public ListNode sortList(ListNode head) {
+         if(head == null || head.next == null) 
+            return head;
+
+        Priority Queue 연습을 위해 구현
         PriorityQueue<ListNode> pq = new PriorityQueue<> (new Comparator<ListNode> () {
             @Override
             public int compare(ListNode a, ListNode b) {
                 return a.val - b.val;
             }
         });
-        
+
         while(head !=null){
             pq.add(head);
             head = head.next;
         }
-        
+
         ListNode newHead = pq.poll();
         ListNode result = newHead;
         while(pq.isEmpty() == false) {
@@ -31,6 +79,7 @@ class Solution {
             newHead = newHead.next;
         }
         newHead.next =null;
-        return result;
+        return reulst;
     }
+    */
 }
