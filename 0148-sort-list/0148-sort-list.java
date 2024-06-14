f7586f9ee4f9e759198fbb25d4d61381105ff3af
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-06-14 19:06:00 +0900

    Time: 9 ms (93.61%), Space: 57 MB (27.66%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -33,7 +33,8 @@ class Solution {
       }
 
       ListNode merge(ListNode l1, ListNode l2) {
-        ListNode l = new ListNode(0), p = l;
+        ListNode l = new ListNode(0);
+        ListNode p = l;
 
         while (l1 != null && l2 != null) {
           if (l1.val < l2.val) {
