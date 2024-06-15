749317788ad0814ca379be72977747cdac089107
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-06-15 16:04:14 +0900

    Time: 41 ms (94.18%), Space: 113.1 MB (66.56%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,3 +1,78 @@
+//직접 구현
+class LinkedNode {
+  int key;
+  int value;
+  LinkedNode prev;
+  LinkedNode next;
+}
+
+public class LRUCache {
+    private HashMap<Integer, LinkedNode> cache = new HashMap<Integer, LinkedNode>();
+    private int capacity;
+    private LinkedNode head =  new LinkedNode();
+    private LinkedNode tail = new LinkedNode();
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+    }
+
+    private void addNode(LinkedNode node) {
+        node.prev = this.head;
+        node.next = this.head.next;
+        
+        this.head.next.prev = node;
+        this.head.next = node;
+    }
+
+    private void removeNode(LinkedNode node){
+        LinkedNode prev = node.prev;
+        LinkedNode next = node.next;
+
+        prev.next = next;
+        next.prev = prev;
+    }
+
+
+    private void moveToHead(LinkedNode node){
+        //Node 삭제
+        this.removeNode(node);
+        //Head 다시 넣기
+        this.addNode(node);
+    }
+
+
+    public int get(int key){
+        if(this.cache.containsKey(key) == false)
+            return -1;
+
+        LinkedNode node = cache.get(key);
+        this.moveToHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value){
+        if(this.cache.containsKey(key) == true) {
+            LinkedNode node = cache.get(key);
+            node.value = value;
+            this.moveToHead(node);
+        } else {
+            LinkedNode newNode = new LinkedNode();
+            newNode.key = key;
+            newNode.value = value;
+            this.cache.put(key, newNode);
+            this.addNode(newNode);
+            if(this.cache.size() > capacity) {
+                LinkedNode removeNode = this.tail.prev;
+                this.removeNode(removeNode);
+                this.cache.remove(removeNode.key);
+            }
+        }
+    }
+}
+
+/*LinkedHashMap 활용
 class LRUCache {
     private int capacity;
     LinkedHashMap<Integer, Integer> cache = new LinkedHashMap<Integer, Integer>();
@@ -27,7 +102,7 @@ class LRUCache {
             cache.remove(removeKey);
         }
     }
-}
+}*/
 
 
 /**
