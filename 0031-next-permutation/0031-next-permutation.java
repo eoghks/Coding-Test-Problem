42c1b0d29264b63309c58da2a712607e88a36bd3
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-06-26 22:12:20 +0900

    Time: 0 ms (100.00%), Space: 43.4 MB (5.14%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,41 +1,34 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int len = nums.length;
-        int idx = 0;
-        boolean isDesc = true;
-        
-        for(int i = len-1; i > 0; i--) {
-            // 오름 차순이 끝나는 곳 파악
-            if(nums[i] > nums[i-1]) {
-                idx = i -1;
-                isDesc = false;
-                break;
-            }
+        int i = nums.length - 2;
+        while(i>=0 && nums[i] >= nums[i+1]){
+            i--;
         }
-        if(isDesc == true) {
-            //내림 차순이라면 다음 행렬을 오름 차순이다.
-            Arrays.sort(nums);
-        } else if(idx == len -2) {
-            //오름차순이 끝이 배열의 길이라면 마지막 두값만 바꿔주면됨
-            int temp = nums[idx];
-            nums[idx] = nums[idx + 1];
-            nums[idx + 1] = temp;
-        } else {
-            int[] nums1 = Arrays.copyOfRange(nums, idx + 1, len);
-            Arrays.sort(nums1);
-            boolean ch = true;
-            int val = nums[idx];
-            System.out.println(val);
-            for(int i =0; i < nums1.length; i ++) {
-                if(ch == true && nums1[i] > val) {
-                    System.out.println(nums1[i]);
-                    nums[idx] = nums1[i];
-                    nums[idx + 1 + i] = val;
-                    ch = false;
-                } else {
-                    nums[idx + 1 + i] = nums1[i];
-                }
+        
+        if(i >= 0) {
+            int j = nums.length-1;
+            //nums[i]보다 큰 요소 중 처음 찾기(내림 차순으로 되어 있기때문에 처음으로 큰애가 큰애중 제일 작은애)
+            while(j >= 0 && nums[i] >= nums[j]){
+                j--;
             }
+            swap(nums,i, j);
+        }
+        
+        changeAsc(nums, i+1);
+    }
+    
+    public void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public void changeAsc(int[] nums,int start){
+        int end = nums.length-1;
+        while(start < end){
+            swap(nums,start,end);
+            start++;
+            end--;
         }
     }
 }
