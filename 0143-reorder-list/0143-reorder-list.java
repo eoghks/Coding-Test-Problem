1d3e0d98a8daab7b48a15a37ed7a780bedc32316
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-06-30 16:15:04 +0900

    Time: 2 ms (82.15%), Space: 47.9 MB (67.83%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -12,6 +12,7 @@ class Solution {
     public void reorderList(ListNode head) {
         if(head == null || head.next == null)
             return;
+        /*
         Deque<ListNode> deq = new ArrayDeque<ListNode>();
         ListNode dummy = head;
         while(dummy != null) {
@@ -29,7 +30,33 @@ class Solution {
             dummy = temp;
         }
        
-        deq.pollLast().next = null;
+        deq.pollLast().next = null;*/
+          ListNode fast = head, slow = head;
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }//slow is preMid
+        
+        ListNode preMid = slow;
+        ListNode cur = slow.next;//cur is mid
+        while(cur.next != null){
+            ListNode next = cur.next;
+            cur.next = next.next;
+            next.next = preMid.next;
+            preMid.next = next;
+        }
+        
+        ListNode p1 = head;
+        ListNode p2 = preMid.next;
+        
+        while(p1 != preMid){
+            preMid.next = p2.next;
+            p2.next = p1.next;
+            p1.next = p2;
+            p1 = p2.next;
+            p2 = preMid.next;
+        }
     }
+    
 
 }
