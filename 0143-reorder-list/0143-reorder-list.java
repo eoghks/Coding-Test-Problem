56ea41aa0b393a0c7c08e7c27638e46c29bad35c
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-06-30 16:16:34 +0900

    Time: 4 ms (16.73%), Space: 48.6 MB (6.98%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -12,33 +12,16 @@ class Solution {
     public void reorderList(ListNode head) {
         if(head == null || head.next == null)
             return;
-        /*
-        Deque<ListNode> deq = new ArrayDeque<ListNode>();
-        ListNode dummy = head;
-        while(dummy != null) {
-            deq.add(dummy);
-            dummy = dummy.next;
-        }
-
-        int len = (deq.size() - 1) / 2;
-        dummy = head;
-        for(int i =0; i < len; i++) {
-            ListNode last = deq.pollLast();
-            ListNode temp = dummy.next;
-            dummy.next = last;
-            last.next = temp;
-            dummy = temp;
-        }
-       
-        deq.pollLast().next = null;*/
-          ListNode fast = head, slow = head;
+        /* Runtime 시간을 줄이는 코드(참고용)
+        ListNode fast = head, slow = head;
         while(fast.next != null && fast.next.next != null){
             fast = fast.next.next;
             slow = slow.next;
-        }//slow is preMid
+        }
         
         ListNode preMid = slow;
-        ListNode cur = slow.next;//cur is mid
+        ListNode cur = slow.next;
+        //한칸씩 중간에 끼워넣기
         while(cur.next != null){
             ListNode next = cur.next;
             cur.next = next.next;
@@ -56,6 +39,26 @@ class Solution {
             p1 = p2.next;
             p2 = preMid.next;
         }
+        */
+        Deque<ListNode> deq = new ArrayDeque<ListNode>();
+        ListNode dummy = head;
+        while(dummy != null) {
+            deq.add(dummy);
+            dummy = dummy.next;
+        }
+
+        int len = (deq.size() - 1) / 2;
+        dummy = head;
+        for(int i =0; i < len; i++) {
+            ListNode last = deq.pollLast();
+            ListNode temp = dummy.next;
+            dummy.next = last;
+            last.next = temp;
+            dummy = temp;
+        }
+       
+        deq.pollLast().next = null;
+       
     }
     
 
