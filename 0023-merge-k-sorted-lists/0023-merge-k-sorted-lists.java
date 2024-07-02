29750bd61d206f1361d00213563eb24b035a6abe
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-07-02 13:51:07 +0900

    Time: 4 ms (74.94%), Space: 43.9 MB (96.25%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode result = new ListNode();
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a ,b) -> a.val-b.val);
+        for(ListNode list : lists) {
+            if(list != null) {
+                pq.add(list);
+            }
+        }
+        
+        ListNode dummy = result;
+        while(pq.isEmpty() == false) {
+            dummy.next = pq.remove();
+            dummy = dummy.next;
+            if(dummy.next != null)
+                pq.add(dummy.next);
+        }
+        
+        return result.next;
+    }
+}
+//3개가 들어오면 2개로 합쳐...
