3bb9ffa4d93066833f53573c1bd41e872048a752
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-07-06 17:26:11 +0900

    Time: 24 ms (5.23%), Space: 58.5 MB (45.12%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        long  size =  IntStream.of(nums).distinct().count();
+        if(size== nums.length)
+            return false;
+        return true;
+    }
+}
