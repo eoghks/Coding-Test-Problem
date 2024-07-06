25cb4cc192128bf813f19ff011b1cc4d452529b4
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-07-06 17:26:16 +0900

    Time: 23 ms (5.79%), Space: 58.4 MB (45.12%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        long  size =  IntStream.of(nums).distinct().count();
+        long size = IntStream.of(nums).distinct().count();
         if(size== nums.length)
             return false;
         return true;
