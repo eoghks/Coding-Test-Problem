6dfe25c013e573adc9e7ad14d400ca42d8339ee5
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-07-27 12:53:38 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (19.45%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,16 +1,16 @@
 class Solution {
     public int countVowelStrings(int n) {
-        int[][] dp = new int[n+1][5];
+        int[][] dp = new int[n][5];
         for(int i=0; i < 5; i++){
-            dp[1][i] = 1;
+            dp[0][i] = 1;
         }
-        for(int i = 2; i <= n; i++) {
+        for(int i = 1; i < n; i++) {
             dp[i][0] = 1;
             for(int j = 1; j < 5; j++) {
                 dp[i][j] = dp[i][j-1] + dp[i-1][j];
             }
         }
         
-        return dp[n][0] + dp[n][1] + dp[n][2] + dp[n][3] + dp[n][4];
+        return dp[n-1][0] + dp[n-1][1] + dp[n-1][2] + dp[n-1][3] + dp[n-1][4];
     }
 }
