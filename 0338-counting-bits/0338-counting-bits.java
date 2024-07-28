3f95e2dcd40e9b497c05b3e514b9395e7b7b15b1
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-07-29 00:29:38 +0900

    Time: 2 ms (93.41%), Space: 46.1 MB (94.08%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,17 +1,8 @@
 class Solution {
     public int[] countBits(int n) {
         int[] dp = new int[n+1];
-        dp[0] = 0;
-        int j = 0;
-        int k = 1;
         for(int i = 1; i<=n; i++) {
-            dp[i] = 1 + dp[j];
-            if(i == Math.pow(2, k) -1) {
-                k++;
-                j = 0;
-            } else {
-                j++;
-            }
+            dp[i] = dp[i >> 1] +(i&1);
         }
         return dp;
     }
