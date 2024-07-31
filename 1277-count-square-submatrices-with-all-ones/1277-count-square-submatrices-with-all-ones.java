418f234c435123119e9d42f6cacca7ae1b0752e6
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-07-31 16:08:32 +0900

    Time: 7 ms (44.12%), Space: 56.8 MB (5.32%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,4 +1,10 @@
 class Solution {
+    /*
+        자기 자신이 오른쪽 아래를 기준으로 만들어지는 정사각형의 갯수를 구한다.
+        0 1 1 1    0 1 1 1    0 1 1 1
+        1 1 1 1 -> 1 1 2 2 -> 1 1 2 2 이된다. 
+        0 1 1 1    0 1 1 1    0 1 2 3  
+    */
     public int countSquares(int[][] matrix) {
         int result = 0;
         for(int i = 0; i < matrix.length; i++) {
