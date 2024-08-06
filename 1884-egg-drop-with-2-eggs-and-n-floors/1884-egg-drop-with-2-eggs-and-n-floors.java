ae5d7fd4f64c7029ef9eeabd63e19a4ecfd7e992
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-08-06 12:02:29 +0900

    Time: 0 ms (100.00%), Space: 40.3 MB (80.17%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.java
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.java
@@ -1,8 +1,8 @@
 class Solution {
     public int twoEggDrop(int n) {
         int result = 1;
-        n = n - 2 ; //층 번던져서 3층까지 확인가능
-        while(n >= 0) {
+        n = n - 2 ; // 0층에서 한번 던졋을 때, 깨지면 0층 안깨지며 1층에서 깨진다는것을 알 수 있다.
+        while(n >= 0) {// >=를 사용한 이뉴는 n층이라하지만 0층이 존재하여 실제로는 n+1층이 존재한다.
             result++;
             n -= result;
         }
