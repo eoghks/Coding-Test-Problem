a9eea3c3ad7b5d7953cd82423a5898b32bb6d92e
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-08-10 12:57:37 +0900

    Time: 18 ms (46.96%), Space: 42.7 MB (42.94%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.java
+++ 1395-count-number-of-teams/1395-count-number-of-teams.java
@@ -1,21 +1,24 @@
 class Solution {
     public int numTeams(int[] rating) {
         int cnt = 0;
-        boolean[][] dp = new boolean[rating.length][rating.length];
+        int[] small = new int[rating.length];
+        int[] large = new int[rating.length];
         
-        for(int i = 0; i < rating.length -1; i++) {
-            for(int j = i+1; j< rating.length; j++) {
-                if(rating[i] < rating[j])
-                    dp[i][j] = true;
+        for(int i = 0; i < rating.length; i++) {
+            for(int j = 0; j< i; j++) {
+                if(rating[j] < rating[i])
+                    large[i]++;
+                else if(rating[j] > rating[i]) 
+                    small[i]++;
             }
         }
         
-        for(int i = 0; i < rating.length -2; i++) {
-            for(int j = i+1; j<rating.length-1; j++) {
-                for(int k = j+1; k<rating.length; k++) {
-                    if(dp[i][j] == dp[j][k])
-                        cnt++;
-                }
+        for(int i = 0; i < rating.length; i++) {
+            for(int j = 0; j < i; j++) {
+                if(rating[j] < rating[i])
+                    cnt += large[j];
+                else if(rating[j] > rating[i]) 
+                    cnt += small[j];
             }
         }
         
