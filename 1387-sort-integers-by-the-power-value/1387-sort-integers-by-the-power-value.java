0a1836fb87989d34788d3fdf296bcf3f7e41abd9
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-08-11 20:28:19 +0900

    Time: 44 ms (50.93%), Space: 44.8 MB (23.26%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.java
@@ -0,0 +1,47 @@
+class Solution {
+    public class Node implements Comparable<Node> {
+        int key;
+        int val;
+        
+        public Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+        
+        @Override
+        public int compareTo(Node n) {
+            if(this.val > n.val )
+                return 1;
+            else if(this.val < n.val)
+                return -1;
+            else if(this.val == n.val && this.key < n.key)
+                return -1;
+            else
+                return 1;
+        }
+    }
+    
+    Map<Integer, Integer> dp = new HashMap<>();
+    
+    public int getKth(int lo, int hi, int k) {
+        List<Node> list = new ArrayList<>();
+        dp.put(1 , 0);
+        for(int i = lo; i<=hi; i++) {
+            list.add(new Node(i, solution(i)));
+        }
+        Collections.sort(list);
+        
+        return list.get(k-1).key;
+    }
+    
+    public int solution(int n) {
+        if(dp.containsKey(n) == false) {
+            if(n%2 == 0)
+                dp.put(n, solution(n/2)+1);
+            else
+                dp.put(n, solution(3*n+1)+1);
+        }
+        
+        return dp.get(n);
+    } 
+}
