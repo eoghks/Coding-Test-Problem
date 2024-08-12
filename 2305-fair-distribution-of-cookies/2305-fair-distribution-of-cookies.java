847fa6686610c7ce489a27cc2ee27564bd751b39
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-08-12 17:09:28 +0900

    Time: 5 ms (81.30%), Space: 41.1 MB (25.35%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -6,8 +6,12 @@ class Solution {
         soultion(cookies, nums, 0, k);
         return result;
     }
-    
+    //zeroCount -> 현재 0개의 쿠키를 가진 아이들 수
     public void soultion(int[] cookies, int[]nums, int cur, int zeroCount) {
+        //최소 한명의 아이는 하나의 쿠키 가방은 가질 수 있다.
+        //왜냐하면 2 <= k <= cookies.length 이기때문이다.
+        //따라서 현재 0개의 쿠키를 가진 아이들 수가 남은 쿠기 가방 수보다 적은 경우 
+        //아이들은 최소 하나의 쿠키 가방을 가질 수 없다. 따라서 더이상 확인할 필요가 없다.
         if(cookies.length - cur < zeroCount) {
             return;
         }
