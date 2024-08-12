0792e4fa8e6ab3a9272b99d48e319299b90e985f
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-08-12 17:06:47 +0900

    Time: 5 ms (81.30%), Space: 40.5 MB (84.54%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -3,11 +3,15 @@ class Solution {
     int[] nums;
     public int distributeCookies(int[] cookies, int k) {
         nums = new int[k];
-        soultion(cookies, nums, 0);
+        soultion(cookies, nums, 0, k);
         return result;
     }
     
-    public void soultion(int[] cookies, int[]nums, int cur) {
+    public void soultion(int[] cookies, int[]nums, int cur, int zeroCount) {
+        if(cookies.length - cur < zeroCount) {
+            return;
+        }
+        
         if(cur == cookies.length) {
             int temp =0;
             for(int i=0; i<nums.length; i++) {
@@ -20,9 +24,11 @@ class Solution {
         for(int i=0; i<nums.length; i++) {
             if(nums[i] + cookies[cur] > result)
                 continue;
+            zeroCount -= nums[i] == 0? 1:0;
             nums[i]+=cookies[cur];
-            soultion(cookies, nums, cur+1);
+            soultion(cookies, nums, cur+1, zeroCount);
             nums[i]-=cookies[cur];
+            zeroCount += nums[i] == 0? 1:0;
         }
     }
 }
