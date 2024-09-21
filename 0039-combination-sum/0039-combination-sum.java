cdf776ad2ece16574aee7b5ffe95da3fce07c906
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-09-21 10:31:50 +0900

    Time: 2 ms (83.38%), Space: 44.5 MB (68.95%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,26 @@
 class Solution {
     List<List<Integer>> res;
     List<Integer> temp;
+    int[] can;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         res = new ArrayList<>();
         temp = new ArrayList<>();
         Arrays.sort(candidates);
-        solution(candidates, target, 0, 0);
+        can = candidates;
+        solution(target, 0, 0);
         return res;
     }
     
-    public void solution(int[] candidates, int target, int idx, int cur) {
+    public void solution(int target, int idx, int cur) {
         if(cur == target) {
             res.add(new ArrayList<>(temp));
             return;
         } 
         
-        for(int i=idx; i<candidates.length; i++) {
-            if(cur+candidates[i] <= target) {
-                temp.add(candidates[i]);
-                solution(candidates, target, i, cur+candidates[i]);
+        for(int i=idx; i<can.length; i++) {
+            if(cur+can[i] <= target) {
+                temp.add(can[i]);
+                solution(target, i, cur+can[i]);
                 temp.remove(temp.size()-1);
             } else
                 break;
