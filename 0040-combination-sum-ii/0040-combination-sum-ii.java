4d952ff55d8bdca8c37fbe4fc9fabf44e80ce33e
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-09-22 14:30:46 +0900

    Time: 3 ms (77.73%), Space: 43.3 MB (26.05%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,15 +1,18 @@
 class Solution {
+    
     List<List<Integer>> res ;
+    List<Integer> temp;
     int[] candidate;
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         res = new ArrayList<>();
+        temp = new ArrayList<>();
         Arrays.sort(candidates);
         candidate = candidates;
-        solution(target, 0, new ArrayList<>());
+        solution(target, 0);
         return res;
     }
     
-    public void solution(int target, int idx, List<Integer> temp) {
+    public void solution(int target, int idx) {
         if(target == 0 ) {
             if(res.contains(temp) == false)
                 res.add(new ArrayList<>(temp));
@@ -27,7 +30,7 @@ class Solution {
                 if(i > idx && candidate[i] == candidate[i-1])
                     continue;
                 temp.add(candidate[i]);
-                solution(target-candidate[i], i+1, temp);
+                solution(target-candidate[i], i+1);
                 temp.remove(temp.size()-1);
             } else
                 break;
