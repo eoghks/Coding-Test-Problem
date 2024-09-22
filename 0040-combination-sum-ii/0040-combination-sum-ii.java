66a757fa7fc63b2dcac35bef407bf002db94c2b7
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-09-22 14:28:59 +0900

    Time: 3 ms (77.73%), Space: 43 MB (89.41%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -21,6 +21,12 @@ class Solution {
             
         for(int i = idx; i<candidate.length; i++) {
             if(target >= candidate[i]) {
+                //이미 전에서 한번 확인한 경우와 같기 때문에 제외한다.
+                //만약 [1,2,2,2,2,2]와 target이 주어질때
+                //현재 idx =2이고, i는 3이라 가정하자.
+                //이미 전에 i가 2일때 2를 넣고 한바퀴를 돌았다. 즉 뒤에 2,2,2,2로 만들 수 잇는 모든 경우에수는 확인햇다.
+                //i가 3이되면 2를 넣고 2,2,2를 가지고 만들수 있는 모든 경우의 수를 확인한다.
+                //즉, 이미 더큰 집합에서 경우의 수를 다확인하였기 때문에 중복해서 확인할 필요가 없다.
                 if(i > idx && candidate[i] == candidate[i-1])
                     continue;
                 temp.add(candidate[i]);
