1bdf38d2b56a277006897b638b3e93b20389cc0a
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-09-25 17:13:28 +0900

    Time: 1 ms (53.44%), Space: 42.4 MB (93.33%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,19 @@
+class Solution {
+    List<List<Integer>> res;
+    public List<List<Integer>> subsets(int[] nums) {
+        res = new ArrayList<>();
+        solution(new ArrayList<>(), nums, 0);
+        return res;
+    }
+    
+    public void solution(List<Integer> list, int[] nums, int idx) {
+        res.add(new ArrayList<>(list));
+        if(idx==nums.length) 
+            return;
+        for(int i = idx; i<nums.length; i++) {
+            list.add(nums[i]);
+            solution(list, nums, i+1);
+            list.remove(list.size()-1);
+        }
+    }
+}
