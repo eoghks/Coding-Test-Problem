af5d46b1f500e446a6c72fa9d9c435772fb42ccc
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-09-26 13:10:37 +0900

    Time: 21 ms (68.37%), Space: 45.8 MB (23.52%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -6,8 +6,16 @@ class MyCalendar {
     
     public boolean book(int start, int end) {
         Integer smallerKey = map.floorKey(start);
+        //smallerKey == null 
+        //-> 내 행사보다 먼저 시작하는 행사가 없다. == 내 행사가 언제 시작하든 상관이 없다.
+        //map.get(smallerKey) <= start 
+        //-> 나보다 먼저 시작하는 행사 중 제일 마지막에 시작하는 행사가 내 행사 시작일보다 먼저 끝난다.
         if (smallerKey == null || map.get(smallerKey) <= start) {
             Integer greaterKey = map.ceilingKey(start);
+            //greaterKey == null 
+            //-> 내 행사보다 늦게 시작하는 행사가 없다. == 내 행사가 언제 끝나도 상관없다.
+            //greaterKey >= end
+            //-> 내 행사보다 늦게 시작하는 행사 중 제일 먼저 시작하는 행사의 시작일이 내 행사 끝나는 일보다 늦게 시작한다.
             if (greaterKey == null || greaterKey >= end) {
                 map.put(start, end);
                 return true;
