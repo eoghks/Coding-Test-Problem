a67715aa855d8927b99e6962119534bd21f4d338
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-10-01 13:06:18 +0900

    Time: 0 ms (100.00%), Space: 40.8 MB (12.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,22 @@
 class Solution {
     int dp[][];
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+        // Bottom-up
+        int[][] dp = new int[m+1][n+1];
         
-        for(int i=0;i<n;i++)
-            dp[0][i]=1;
-        for(int i=0;i<m;i++)
-            dp[i][0]=1;
+        for(int i=1;i<=n;i++)
+            dp[1][i]=1;
+        for(int i=1;i<=m;i++)
+            dp[i][1]=1;
         
-        for(int i=1;i<m;i++) {
-            for(int j=1;j<n;j++) {
-                
+        for(int i=2;i<=m;i++) {
+            for(int j=2;j<=n;j++) {
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
         
-        return dp[m-1][n-1];
-        /*
+        return dp[m][n];
+        /* Top-Down
         dp = new int[m+1][n+1];
         dp[1][1] = 1;
         return solution(m, n);
