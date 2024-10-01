90c01ae86510a0ee9f5602d6397156d3a28a0433
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-10-01 13:08:47 +0900

    Time: 0 ms (100.00%), Space: 40.3 MB (63.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,30 +1,27 @@
 class Solution {
     int dp[][];
     public int uniquePaths(int m, int n) {
-        // Bottom-up
-        int[][] dp = new int[m+1][n+1];
+        dp = new int[m][n];
         
-        for(int i=1;i<=n;i++)
-            dp[1][i]=1;
-        for(int i=1;i<=m;i++)
-            dp[i][1]=1;
-        
-        for(int i=2;i<=m;i++) {
-            for(int j=2;j<=n;j++) {
+        for(int i=0;i<n;i++)
+            dp[0][i]=1;
+        for(int i=0;i<m;i++)
+            dp[i][0]=1;
+        /* Bottom-up
+        for(int i=1;i<m;i++) {
+            for(int j=1;j<n;j++) {
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
         
-        return dp[m][n];
-        /* Top-Down
-        dp = new int[m+1][n+1];
-        dp[1][1] = 1;
-        return solution(m, n);
+        return dp[m-1][n-1];
         */
+        /* Top-Down*/
+        return solution(m-1, n-1);
     }
     
     public int solution(int m, int n) {
-        if(dp[m][n] == 0 && m != 0 && n != 0)
+        if(dp[m][n] == 0)
             dp[m][n] = solution(m-1, n) + solution(m, n-1);
         return dp[m][n];
     }
