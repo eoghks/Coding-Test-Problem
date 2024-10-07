57aa23a9aa03ad0e9bf7986a203656fbb6c0e515
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-10-07 19:48:09 +0900

    Time: 6 ms (46.21%), Space: 43.3 MB (50.13%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -1,19 +1,5 @@
 class Solution {
     public int minLength(String s) {
-        Stack<Character> st = new Stack<>();
-        int n = s.length();
-        st.push(s.charAt(0));
-        for(int i=1;i<n;i++)
-        {
-            if(!st.isEmpty()&&((s.charAt(i)=='B'&&st.peek()=='A')||(s.charAt(i)=='D'&&st.peek()=='C')))
-            {
-                st.pop();
-            }
-            else
-                st.push(s.charAt(i));
-        }
-        return st.size();
-        /*
         //stack에 이전 char 문자를 기억해가면서 현재 문자와 비교
         Stack<Character> stack = new Stack<>();
         for(char c: s.toCharArray()) {
@@ -23,6 +9,6 @@ class Solution {
                 stack.push(c);
         }
         //stack에 남은 문자 수 반환
-        return stack.size();*/
+        return stack.size();
     }
 }
