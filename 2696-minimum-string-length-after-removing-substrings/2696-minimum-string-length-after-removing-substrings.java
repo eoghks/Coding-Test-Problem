077907113884ffbd657a762c26e4216d5d76d3b2
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-10-07 19:47:13 +0900

    Time: 6 ms (46.21%), Space: 43.3 MB (50.13%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -1,5 +1,6 @@
 class Solution {
     public int minLength(String s) {
+        //stack에 이전 char 문자를 기억해가면서 현재 문자와 비교
         Stack<Character> stack = new Stack<>();
         for(char c: s.toCharArray()) {
             if(stack.isEmpty() == false &&((stack.peek()=='A'&& c=='B') || (stack.peek()=='C'&&c=='D')))
@@ -7,6 +8,7 @@ class Solution {
             else
                 stack.push(c);
         }
+        //stack에 남은 문자 수 반환
         return stack.size();
     }
 }
