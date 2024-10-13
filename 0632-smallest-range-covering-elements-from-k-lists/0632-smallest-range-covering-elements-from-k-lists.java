ba6365af6ff54569ac029a2ef5fdd28717c95a01
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-10-13 17:25:39 +0900

    Time: 28 ms (90.59%), Space: 48.7 MB (65.85%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -7,6 +7,7 @@ class Solution {
         int curMax = Integer.MIN_VALUE;
         int range = Integer.MAX_VALUE;
         
+        //초기 Queue 셋팅
         for(int i=0; i<nums.size(); i++) {
             int val = nums.get(i).get(0);
             Node n = new Node(val, 0, i);
@@ -14,6 +15,7 @@ class Solution {
             curMax = Math.max(val, curMax);
         }
         
+        //nums의 크기보다 queue의 사이즈가 작다면 더이상 비교할 이유가 없음(모든 리스트에서 하나의 원소를 가져와야하므로)
         while(pq.size() == nums.size()) {
             Node n = pq.remove();
             
