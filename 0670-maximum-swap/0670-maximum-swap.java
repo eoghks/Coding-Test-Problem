49f8e55b974ab11994ef099409398861a35126d6
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-10-17 14:14:08 +0900

    Time: 0 ms (100.00%), Space: 40.2 MB (79.95%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -3,6 +3,7 @@ class Solution {
         String s = String.valueOf(num);
         char[] c = s.toCharArray();
         int i = 1;
+        //내림차순이 아닌부분찾기
         while(i<c.length) {
             if(c[i-1] <c[i])
                 break;
@@ -11,11 +12,13 @@ class Solution {
         
         if(i<c.length) {
             int j = i;
+            // 뒤에서 최대값의 인덱스찾기
             for(int k =j+1; k<c.length; k++) {
                 if(c[j] <= c[k])
                     j = k;
             }
             
+            //뒤에서 최대값 보다 작은 교환할 값의 인덱스 찾아 바꾸기
             for(int k=0; k<i; k++) {
                 if(c[k] <c[j]) {
                     char temp = c[k];
