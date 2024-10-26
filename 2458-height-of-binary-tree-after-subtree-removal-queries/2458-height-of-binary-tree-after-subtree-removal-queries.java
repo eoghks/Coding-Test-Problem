09c1807c56df40d913b240343b2c79c77ecfffc5
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-10-26 15:20:50 +0900

    Time: 81 ms (42.46%), Space: 106.6 MB (8.42%) - LeetHub

---
+++ 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private Map<Integer, Integer> leftMap = new HashMap<>();
+    private Map<Integer, Integer> rightMap = new HashMap<>();
+    private Map<Integer, Integer> removed = new HashMap<>();
+
+    public int[] treeQueries(TreeNode root, int[] queries) {
+        int[] res = new int[queries.length];
+        getHeight(root, 0);
+        getRemoveHeight(root, 0);
+        for(int i=0; i<queries.length; i++) {
+            res[i] = removed.get(queries[i]);
+        }
+        return res;
+    }
+    
+    private void getRemoveHeight(TreeNode node, int height) {
+        if(node == null)
+            return;
+        removed.put(node.val, height);
+        getRemoveHeight(node.left, Math.max(height, rightMap.get(node.val)));
+        getRemoveHeight(node.right, Math.max(height, leftMap.get(node.val)));
+    }
+    
+    
+    private int getHeight(TreeNode node, int height) {
+        if(node == null)
+            return height -1;
+        //왼쪽 서브트리의 높이를 구하기
+        leftMap.put(node.val, getHeight(node.left, height +1));
+        //오른쪽 서브트리의 높이를 구하기
+        rightMap.put(node.val, getHeight(node.right, height +1));
+        
+        //더 높이가 높은 쪽의 서브트리의 높이가 나의 높이
+        return Math.max(leftMap.get(node.val), rightMap.get(node.val));
+    }
+}
