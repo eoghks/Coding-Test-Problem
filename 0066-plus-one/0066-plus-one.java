b65e9728c0c53ed0846485272adfefb00ee37c05
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-10-26 15:31:35 +0900

    Time: 0 ms (100.00%), Space: 41.9 MB (19.03%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -8,6 +8,7 @@ class Solution {
             digits[i] = 0;
         }
         
+        //기존 자릿수가 모두 1이고 올림수가 1이 남앗다.
         digits = new int[digits.length + 1];
         digits[0] = 1;
         return digits;
