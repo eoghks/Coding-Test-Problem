2aab44ee4a5c54433ba72bc1ed66b76cc3dcba62
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-10-31 16:59:51 +0900

    Time: 35 ms (80.00%), Space: 42.9 MB (78.46%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
@@ -1,4 +1,6 @@
 class Solution {
+    //dp1 114ms, 57.1MB
+    /*
     int n;
     int m;
     long[][] dp;
@@ -42,5 +44,54 @@ class Solution {
         dp[i][j] = Math.min(cost1, cost2);
         return dp[i][j];
         
+    }*/
+    
+    //dp2
+    int n;
+    int m;
+    long[][] dp;
+    public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
+        Collections.sort(robot);
+        Arrays.sort(factory, (a,b) -> a[0] -b[0]);
+
+        n = robot.size();
+        m = factory.length;
+        dp = new long[n][m];
+
+        return solution(0, 0, robot, factory);
+    }
+    
+    public long solution(int i,int j, List<Integer> robot, int[][] factory) {
+        if (i == n) {
+            return 0;
+        }
+
+        if (j == m) {
+            return Long.MAX_VALUE;
+        }
+        
+        if(dp[i][j] != 0){
+            return dp[i][j];
+        }
+
+        long res = solution(i, j + 1, robot, factory);
+
+        long sum = 0;
+        int cnt = 0;
+        for (int k = i; k < n; k++) {
+            int cur = Math.abs(robot.get(k) - factory[j][0]);
+            if (cnt < factory[j][1]) {
+                long next = solution(k + 1, j + 1, robot, factory);
+                if(next!=Long.MAX_VALUE) {
+                  next +=  sum + cur;
+                }
+                res = Math.min(res, next);
+                cnt++;
+                sum += cur;
+            } else {
+                break;
+            }
+        }
+        return dp[i][j] = res;
     }
 }
