0b89ce4d559af15823c2e2595e6413c3e95081aa
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-11-11 16:07:41 +0900

    Time: 7 ms (18.42%), Space: 44.8 MB (41.72%) - LeetHub

--- 2601-prime-subtraction-operation/2601-prime-subtraction-operation.java
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.java
@@ -1,4 +1,6 @@
 class Solution {
+    //solution1
+    /*
     public boolean primeSubOperation(int[] nums) {
         List<Integer> primes = getPrims(1000);
         for(int i = nums.length-1; i>=1; i--) {
@@ -18,6 +20,33 @@ class Solution {
             }
         }
         return true;
+    }*/
+    
+    //soultion2 ->binary search
+    List<Integer> primes;
+    public boolean primeSubOperation(int[] nums) {
+        primes = getPrims(1000);
+        int prev = 0;
+        for(int n: nums) {
+            if(prev >= n)
+                return false;
+            prev = n - search(n-prev);
+        }
+        return true;
+    }
+    
+    private int search(int n) {
+        int st = 0;
+        int end = primes.size()-1;
+        while(st < end) {
+            int mid = (st + end)/2;
+            if(n>primes.get(mid)) {
+                st = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return primes.get(st) >=n? primes.get(st-1) : primes.get(st);
     }
     
     private List<Integer> getPrims(int num) {
@@ -25,6 +54,7 @@ class Solution {
         boolean[] primes = new boolean[num+1];
         
         int i=2;
+        res.add(0);
         for(; i*i<=num; i++) {
             if(!primes[i]) {
                 for(int j= i*2; j<=num; j=j+i) {
