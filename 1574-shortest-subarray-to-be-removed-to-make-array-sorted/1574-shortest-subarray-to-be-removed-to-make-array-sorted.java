cc623bd44bc684906a3c549c4177f27b7c370176
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-11-15 16:44:16 +0900

    Time: 1 ms (100.00%), Space: 65.2 MB (22.86%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.java
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.java
@@ -1,22 +1,5 @@
 class Solution {
     public int findLengthOfShortestSubarray(int[] arr) {
-        int len = arr.length;
-        int l = 0, r = len - 1;
-        while(l + 1 < len && arr[l + 1] >= arr[l]) l++;
-        while(r - 1 >= 0 && arr[r - 1] <= arr[r]) r--;
-        if(r <= l) return 0;
-        int st = r;
-        while(st < len && arr[st] < arr[0]) st++;
-        int ans = 0;
-        if(st >= len) return len - Math.max(l + 1, len - r);
-        ans = Math.max(ans, 1 + len - st);
-        for(int i = 1; i <= l; i++) {
-            while(st < len && arr[st] < arr[i]) st++;
-            if(st >= len) break;
-            ans = Math.max(ans, 1 + i + len - st);
-        }
-        return len - Math.max(ans, Math.max(l + 1, len - r));
-        /*
         int n = arr.length;
         int l = 0;
         int r = n-1;
@@ -26,26 +9,28 @@ class Solution {
         while(r-1>=0 && arr[r-1] <= arr[r])
             r--;
         
+        //내림차순이 존재하지 않는 경우
         if(r<=l)
             return 0;
         
         int st = r;
         while(st<n && arr[st] < arr[0])
             st++;
+        
+        //내림 차순 부분을 제거하려는데 
+        //마지막으로 첫번째 원소보다 뒷 부분이 모두 작은 경우
         if(st>=n)
             return n- Math.max(l+1, n-r);
         
-        int res = 1+n-st;
+        int res = Math.max(0, 1+n-st);
         for(int i=1; i<=l; i++) {
             while(st<n&& arr[st] < arr[i])
                 st++;
             if(st >= n)
                 break;
-            res = Math.max(res, l+i+n-st);
+            res = Math.max(res, 1 + i + n - st);
         }
-        System.out.println(l);
-        System.out.println(r);
-        return n-Math.max(res, Math.max(l+1, n-r));*/
+        return n - Math.max(res, Math.max(l + 1, n - r));
     }
     
 }
