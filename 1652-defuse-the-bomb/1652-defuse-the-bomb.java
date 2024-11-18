13e5682b1a9fa8c0a42a3474428e28cbc3553c10
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-11-18 11:57:05 +0900

    Time: 0 ms (100.00%), Space: 42.2 MB (55.38%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] res = new int[n];
+        if(k==0) 
+            return res;
+        
+        int sum = 0;
+        int end = k;
+        int st = 1;
+        
+        if(k<0) {
+            k = -k;
+            st = n - k;
+            end = n - 1;
+        }
+        
+        for(int i = st; i<=end; i++) {
+            sum+=code[i];
+        }
+        
+        for(int i=0; i< n; i++) {
+            res[i] = sum;
+            sum = sum - code[(st++) %n] + code[(++end) %n];
+        }
+        return res;
+    }
+}
