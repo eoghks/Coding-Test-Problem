8f68da2ce24c352c43d4aaa6775276e2b97248df
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-12-05 15:42:03 +0900

    Time: 103 ms (5.29%), Space: 45.9 MB (18.75%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.java
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.java
@@ -3,12 +3,12 @@ class Solution {
         if (!(start.replaceAll("_", "")).equals(target.replaceAll("_", ""))) {
             return false;
         }
-        for (int i = 0, j = 0, n = start.length(); i < n && j < n; ++i, ++j) {
+        for (int i = 0, j = 0, n = start.length(); i < n && j < n; i++, j++) {
             while (i < n && start.charAt(i) == '_') {
-                ++i;
+                i++;
             }
             while (j < n && target.charAt(j) == '_') {
-                ++j;
+                j++;
             }
             if (i < n && j < n && (start.charAt(i) == 'L' && i < j || target.charAt(j) == 'R' && i > j)) {
                 return false;
