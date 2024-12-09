39be10f14d72d4eec9d41fc957d9faa5cc80d388
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-12-09 13:59:13 +0900

    Time: 3 ms (86.83%), Space: 98.4 MB (64.58%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.java
+++ 3152-special-array-ii/3152-special-array-ii.java
@@ -2,6 +2,8 @@ class Solution {
     public boolean[] isArraySpecial(int[] nums, int[][] queries) {
         boolean[] res = new boolean[queries.length];
         int[] temp = new int[nums.length];
+        //연속적으로 홀수(짝수)가 나오면 1을 더함
+        //temp[i]는 0부터 i번째 index까지 인접합 원소가 짝수나 홀수인 경우의 수
         for(int i=1; i<nums.length; i++) {
             temp[i] = temp[i-1];
             if(nums[i-1]%2 == nums[i]%2) {
