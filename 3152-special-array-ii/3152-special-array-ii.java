4e023c70f52579f42a3b475afe4cf044b3eb2eae
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-12-09 14:00:29 +0900

    Time: 3 ms (86.83%), Space: 98 MB (91.15%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.java
+++ 3152-special-array-ii/3152-special-array-ii.java
@@ -10,7 +10,9 @@ class Solution {
                 temp[i]++;
             }
         }
-        
+        //0부터 end까지 special한 경우에서 0부터 st까지 special한 경우를 뺀건
+        //st부터 end까지 special 경우의 수
+        //따라서, 해당 값이 0이면 해당 subarray는 special하다고 할수 있다.
         for(int i=0; i<queries.length; i++) {
             int st = queries[i][0];
             int end = queries[i][1];
