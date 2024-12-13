89343659c9750a07276fc2c97eb47b6f8583abf8
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-12-13 09:45:23 +0900

    Time: 192 ms (27.94%), Space: 58 MB (68.06%) - LeetHub

---
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long findScore(int[] nums) {
+        long res = 0;
+        int n = nums.length;
+        boolean[] mark = new boolean[n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]));
+        
+        for (int i = 0; i < n; i++) {
+            pq.add(new int[] {nums[i], i});
+        }
+        
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            if (!mark[cur[1]]) {
+                mark[cur[1]] = true;
+                res += cur[0];
+                if (cur[1] - 1 >= 0) {
+                    mark[cur[1] - 1] = true;
+                }
+                if (cur[1] + 1 < n) {
+                    mark[cur[1] + 1] = true;
+                }
+            }
+        }
+        return res;
+    }
+}
