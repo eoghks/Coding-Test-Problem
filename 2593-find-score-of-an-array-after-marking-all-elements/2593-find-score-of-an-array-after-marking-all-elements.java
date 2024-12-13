9c4b4fd973d205f281067a4d7c7586061997add9
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-12-13 09:45:58 +0900

    Time: 178 ms (50.49%), Space: 56.9 MB (93.52%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
@@ -5,6 +5,7 @@ class Solution {
         boolean[] mark = new boolean[n];
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]));
         
+        //init pq
         for (int i = 0; i < n; i++) {
             pq.add(new int[] {nums[i], i});
         }
