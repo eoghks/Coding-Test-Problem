c31acf9290f8913df39bdf607ca7914803bd4891
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-12-14 17:17:49 +0900

    Time: 45 ms (29.29%), Space: 58.8 MB (54.93%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.java
+++ 2762-continuous-subarrays/2762-continuous-subarrays.java
@@ -8,7 +8,9 @@ class Solution {
         while(i<n) {
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
             i++;
+            //제일 작은 키와 제일 큰키를 비교
             while(Math.abs(map.firstEntry().getKey()-map.lastEntry().getKey())>2) {
+                //j는 키와 상관이 없다.
                 map.put(nums[j],map.getOrDefault(nums[j],0)-1);
                 if(map.get(nums[j])==0){
                     map.remove(nums[j]);
