8e5474198fd4acea8e43b71afb954f1719ad3dab
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-12-19 14:27:13 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (92.46%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
@@ -2,11 +2,13 @@ class Solution {
     public int maxChunksToSorted(int[] arr) {
         int[] max = new int[arr.length];
         max[0] = arr[0];
+        //i까지 나온 최대 수를 구함
         for(int i=1; i<arr.length; i++) {
             max[i] = Math.max(max[i-1], arr[i]);
         }
         
         int cnt = 0;
+        //max[i]가 i이라는건 0~i까지 0~i까지의 수가 존재한다는 의미
         for(int i=0; i<arr.length; i++) {
             if(max[i] == i)
                 cnt++;
