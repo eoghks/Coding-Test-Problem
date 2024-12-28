86aeb42c9fe5687511de0cf92a0602cd651afd01
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-12-28 20:04:07 +0900

    Time: 3 ms, Memory: 46.5 MB - LeetHub

--- 2225-133-689-maximum-sum-of-3-non-overlapping-subarrays/2225-133-689-maximum-sum-of-3-non-overlapping-subarrays.java
+++ 2225-133-689-maximum-sum-of-3-non-overlapping-subarrays/2225-133-689-maximum-sum-of-3-non-overlapping-subarrays.java
@@ -1,4 +1,5 @@
 class Solution {
+    //lethub test
     //Key는 sbuArray의 합이 최대인 3가지를 구한다는것은 3요소의 합도 최대라는 의미이다. 
     public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
         if (nums == null || nums.length < k * 3)
