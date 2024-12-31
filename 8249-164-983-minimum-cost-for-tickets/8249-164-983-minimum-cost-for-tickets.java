32c94e61029378d4aceae8b3be6ee6ecda07e860
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2024-12-31 18:26:13 +0900

    Time: 1 ms, Memory: 41.7 MB - LeetHub

---
+++ 8249-164-983-minimum-cost-for-tickets/8249-164-983-minimum-cost-for-tickets.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        int end = days[days.length-1];
+        boolean[] travel = new boolean[end+1];
+        for(int day: days)
+            travel[day] = true;
+
+        int[] dp = new int[end+1];
+        dp[0] = 0;
+
+        for(int i=1; i<=end; i++) {
+            if(travel[i] == false) {
+                dp[i] = dp[i-1];
+                continue;
+            }
+
+            //1일권 구매
+            dp[i] = costs[0] + dp[i-1];
+            //7일권 구매
+            dp[i] = Math.min(dp[Math.max(0, i - 7)] + costs[1], dp[i]);
+            //30일권 구매
+            dp[i] = Math.min(dp[Math.max(0, i - 30)] + costs[2], dp[i]);
+        }
+
+        return dp[end];
+    }
+}
