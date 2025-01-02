8268104359d7b1597a65aa53723c093f2839aa17
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-01-02 11:59:16 +0900

    Time: 5 ms, Memory: 86 MB - LeetHub

--- 0523-31-2559-count-vowel-strings-in-ranges/0523-31-2559-count-vowel-strings-in-ranges.java
+++ 0523-31-2559-count-vowel-strings-in-ranges/0523-31-2559-count-vowel-strings-in-ranges.java
@@ -1,6 +1,7 @@
 class Solution {
     //queries[i] = [li, ri] => 0에서부터 ri까지의 조건을 만족하는 요소 수 - 0에서부터 ri-1까지 조건을 만족하는 요소의 수
     public int[] vowelStrings(String[] words, int[][] queries) {
+        //0에서부터 i까지 조건을 만족하는 요소 수
         int[] dp = new int[words.length+1];
         for(int i=0; i<words.length; i++) {
             String s = words[i];
