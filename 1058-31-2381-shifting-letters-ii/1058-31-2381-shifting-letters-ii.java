fe88055679abba622a3fe54d6b45bb95894acac6
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-01-05 16:20:24 +0900

    Time: 7 ms, Memory: 76.4 MB - LeetHub

---
+++ 1058-31-2381-shifting-letters-ii/1058-31-2381-shifting-letters-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String shiftingLetters(String s, int[][] shifts) {
+        int n = s.length();
+        int[] shift = new int[n];
+
+        for(int[] sh: shifts) {
+            shift[sh[0]] += (sh[2] == 1? 1 : -1);
+            if (sh[1] + 1 < n) 
+                shift[sh[1] + 1] -= (sh[2] == 1 ? 1 : -1);
+        }
+
+        //각 index마다 움직일 거리 구하기
+        int cur = 0;
+        for (int i = 0; i < n; ++i) {
+            cur += shift[i];
+            shift[i] = cur;
+        }
+
+        StringBuilder res = new StringBuilder(s);
+        for(int i=0; i<n; i++) {
+            int sh = (shift[i] % 26 + 26) % 26;
+            res.setCharAt(i, (char) ('a' + (s.charAt(i) - 'a' + sh) % 26));
+        }
+        return res.toString();
+    }
+}
