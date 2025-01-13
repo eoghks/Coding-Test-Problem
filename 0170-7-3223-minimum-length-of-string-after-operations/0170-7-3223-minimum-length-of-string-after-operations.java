a9a05731be19199c15aab9647d5c00e4d5bafb22
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-01-13 12:34:27 +0900

    Time: 10 ms, Memory: 46.3 MB - LeetHub

--- 0170-7-3223-minimum-length-of-string-after-operations/0170-7-3223-minimum-length-of-string-after-operations.java
+++ 0170-7-3223-minimum-length-of-string-after-operations/0170-7-3223-minimum-length-of-string-after-operations.java
@@ -8,6 +8,7 @@ class Solution {
         for(int a: alp) {
             if(a==0)
                 continue;
+            //짝수면 2 --> 3개이상일때 2개씩 지울수있으므로 2개가 될때까지 지울수있음
             if(a%2==0) {
                 res+=2;
             } else{
