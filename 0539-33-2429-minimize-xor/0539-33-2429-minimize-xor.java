aaa3c9fa04c1c87a8702054a35424f2dbc7969e0
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-01-15 13:46:41 +0900

    Time: 0 ms, Memory: 40.3 MB - LeetHub

--- 0539-33-2429-minimize-xor/0539-33-2429-minimize-xor.java
+++ 0539-33-2429-minimize-xor/0539-33-2429-minimize-xor.java
@@ -8,12 +8,14 @@ class Solution {
             temp>>=1;
         }
         int res =0;
+        //1인부분 앞에서부터 먼저 채우기
         for(int i =31; i>=0 && countBit > 0; i--) {
             if((num1 & (1<<i)) != 0) {
                 res |= (1 << i);
                 countBit--;
             }
         }
+        //1이 남앗다면 뒤에서부터 채우기
         for(int i =0; i<32 && countBit > 0; i++) {
             if((res & (1<<i)) == 0) {
                 res = res | (1<<i);
