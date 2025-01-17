9adab615f6858acbf5545d1acf8c9810acc46ffb
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-01-17 16:28:03 +0900

    Time: 7 ms, Memory: 60 MB - LeetHub

--- 0479-21-2683-neighboring-bitwise-xor/0479-21-2683-neighboring-bitwise-xor.java
+++ 0479-21-2683-neighboring-bitwise-xor/0479-21-2683-neighboring-bitwise-xor.java
@@ -2,11 +2,13 @@ class Solution {
     public boolean doesValidArrayExist(int[] derived) {
         int n=derived.length;
         int[] org=new int[n];
-    
+        
+        //org 구하기
         for(int i=1; i<n; i++){
             org[i]=derived[i-1]^org[i-1];
         }
 
+        //검증하기
         for(int i=0; i<n; i++){
             int idx=(i+1)%n;
             if(derived[i]!=(org[i]^org[idx])) 
