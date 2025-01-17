0223638781eac0b247332667862f92cdbf80d4aa
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-01-17 16:42:43 +0900

    Time: 2 ms, Memory: 63.1 MB - LeetHub

---
+++ 0481-21-2683-neighboring-bitwise-xor/0481-21-2683-neighboring-bitwise-xor.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        /*
+        int n=derived.length;
+        int[] org=new int[n];
+        
+        //org 구하기
+        for(int i=1; i<n; i++){
+            org[i]=derived[i-1]^org[i-1];
+        }
+
+        //검증하기
+        for(int i=0; i<n; i++){
+            int idx=(i+1)%n;
+            if(derived[i]!=(org[i]^org[idx])) 
+                return false;
+        }
+
+        return true;
+        */
+        int xor = 0;
+        for(int i=0; i<derived.length; i++) {
+            xor^=derived[i];
+        }
+        return xor==0;
+        
+    }
+}
