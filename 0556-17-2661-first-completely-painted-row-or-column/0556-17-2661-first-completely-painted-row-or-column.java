59ba1e8e68e0a8884feb9f55b1a624262dc6ea40
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-01-20 13:52:15 +0900

    Time: 22 ms, Memory: 66 MB - LeetHub

---
+++ 0556-17-2661-first-completely-painted-row-or-column/0556-17-2661-first-completely-painted-row-or-column.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+        int res = -1;
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+        Map<Integer, int[]> map = new HashMap<>();
+
+        Arrays.fill(row, n);
+        Arrays.fill(col, m);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                map.put(mat[i][j], new int[]{i, j});
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int[] pos = map.get(arr[i]);
+            if (--row[pos[0]] == 0 || --col[pos[1]] == 0) {
+                return i;
+            }
+        }
+        return res;
+    }
+}
