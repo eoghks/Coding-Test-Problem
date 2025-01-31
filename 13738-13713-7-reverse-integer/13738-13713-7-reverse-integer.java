243285e4c2a579554dc56718e7a2fa9914bfa8c3
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-01-31 11:16:53 +0900

    Time: 1 ms, Memory: 40.6 MB - LeetHub

---
+++ 13738-13713-7-reverse-integer/13738-13713-7-reverse-integer.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int reverse(int x) {
+        int num = Math.abs(x);
+
+        int res = 0;
+        while (num != 0) {
+            int remainder = num % 10;
+            
+            // 오버플로우
+            if (res > (Integer.MAX_VALUE - remainder) / 10) {
+                return 0; 
+            }
+            
+            res = res * 10 + remainder;  
+            num = num / 10; 
+        }
+
+        return x<0? -res: res;
+    }
+}
