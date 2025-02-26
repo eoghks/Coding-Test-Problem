4ad61f165fb3c6e75e8c298955489d5cc91e9485
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-02-26 12:18:54 +0900

    Time: N/A, Memory: N/A',
      },
      questionId: '1849',
      submissionId: '1555604421',
      sessionId: '5844648',
      getLangDisplay: 'java',
      submissionCode: 'class Solution {\u000A    public int maxAbsoluteSum(int[] nums) {\u000A        int n \u003D nums.length\u003B\u000A        int sum \u003D 0\u003B\u000A        int max \u003D 0\u003B\u000A        int cur \u003D 0\u003B\u000A        int pmax \u003D 0\u003B\u000A        int nmin \u003D 0\u003B\u000A        for(int i\u003D0\u003B i\u003Cn\u003B i++) {\u000A            cur \u003D nums[i]\u003B\u000A            sum+\u003D cur\u003B\u000A            \u000A            if(cur \u003C 0) {\u000A                max \u003D Math.max(max, Math.abs(pmax\u002Dsum))\u003B\u000A                nmin\u003D Math.min(nmin, sum)\u003B\u000A            } else {\u000A                max \u003D Math.max(max,Math.abs(nmin\u002Dsum))\u003B\u000A                pmax \u003D Math.max(pmax, sum)\u003B\u000A            }\u000A        }\u000A        return max\u003B\u000A    }\u000A}',
      editCodeUrl: '/problems/maximum-absolute-sum-of-any-subarray/',
      checkUrl: '/submissions/detail/1555604421/check/',
      runtimeDistributionFormatted: '{\u0022lang\u0022: \u0022java\u0022, \u0022distribution\u0022: [[\u00222\u0022, 2.1164], [\u00223\u0022, 26.1905], [\u00224\u0022, 20.6349], [\u00225\u0022, 7.672], [\u00226\u0022, 15.873], [\u00227\u0022, 14.5503], [\u00228\u0022, 4.2328], [\u00229\u0022, 3.1746]]}',
      memoryDistributionFormatted: '{\u0022lang\u0022: \u0022java\u0022, \u0022distribution\u0022: [[\u002254900\u0022, 0.2646], [\u002255200\u0022, 0.2646], [\u002255400\u0022, 0.2646], [\u002255500\u0022, 0.5291], [\u002255700\u0022, 0.7937], [\u002255900\u0022, 0.7937], [\u002256000\u0022, 1.3228], [\u002256100\u0022, 3.1746], [\u002256200\u0022, 6.6138], [\u002256300\u0022, 8.9947], [\u002256400\u0022, 10.3175], [\u002256500\u0022, 10.3175], [\u002256600\u0022, 13.2275], [\u002256700\u0022, 9.7884], [\u002256800\u0022, 11.6402], [\u002256900\u0022, 7.672], [\u002257000\u0022, 5.291]]}',
      langs: [
        
      ],
      runtime: 'None',
      memory: 'None',
      enableMemoryDistribution: 'True',
      nonSufficientMsg: 'Sorry. We do not have enough accepted submissions to show distribution chart. - LeetHub

--- 1362-23-1749-maximum-absolute-sum-of-any-subarray/1362-23-1749-maximum-absolute-sum-of-any-subarray.java
+++ 1362-23-1749-maximum-absolute-sum-of-any-subarray/1362-23-1749-maximum-absolute-sum-of-any-subarray.java
@@ -1,20 +1,21 @@
 class Solution {
     public int maxAbsoluteSum(int[] nums) {
         int n = nums.length;
-        int presum = 0;
+        int sum = 0;
         int max = 0;
         int cur = 0;
         int pmax = 0;
         int nmin = 0;
         for(int i=0; i<n; i++) {
             cur = nums[i];
-            presum+= cur;
+            sum+= cur;
+            
             if(cur < 0) {
-                max = Math.max(max, Math.abs(pmax-presum));
-                nmin= Math.min(nmin, presum);
+                max = Math.max(max, Math.abs(pmax-sum));
+                nmin= Math.min(nmin, sum);
             } else {
-                max = Math.max(max,Math.abs(nmin-presum));
-                pmax = Math.max(pmax,presum);
+                max = Math.max(max,Math.abs(nmin-sum));
+                pmax = Math.max(pmax, sum);
             }
         }
         return max;
