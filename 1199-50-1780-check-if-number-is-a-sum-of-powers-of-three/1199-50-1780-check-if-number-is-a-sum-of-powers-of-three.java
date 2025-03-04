023ccd390c34affc3138231f5ab4916f6b502f39
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-03-04 16:08:24 +0900

    Time: 0 ms, Memory: 40.9 MB - LeetHub

--- 1199-50-1780-check-if-number-is-a-sum-of-powers-of-three/1199-50-1780-check-if-number-is-a-sum-of-powers-of-three.java
+++ 1199-50-1780-check-if-number-is-a-sum-of-powers-of-three/1199-50-1780-check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,4 +1,7 @@
 class Solution {
+    //3^x가 하나도 포함되지 않거나 1개만 포함되어야 한다.
+    //만약 3개가 포함되면 이는 3^x*3이므로 3개가 포함된 경우는 생각할 필요가 없다.
+    //이는 3으로 나누었을때 나머지가 2이면 안된다는 의미이다.
     public boolean checkPowersOfThree(int n) {
         while(n>0) {
             if(n%3==2)
