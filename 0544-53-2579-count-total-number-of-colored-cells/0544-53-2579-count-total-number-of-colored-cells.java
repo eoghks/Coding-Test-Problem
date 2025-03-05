b9d8dfb460c6417adfc51d39964c2a614578ac9a
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-03-05 18:35:43 +0900

    Time: 0 ms, Memory: 40.4 MB - LeetHub

--- 0544-53-2579-count-total-number-of-colored-cells/0544-53-2579-count-total-number-of-colored-cells.java
+++ 0544-53-2579-count-total-number-of-colored-cells/0544-53-2579-count-total-number-of-colored-cells.java
@@ -1,4 +1,9 @@
 class Solution {
+    //n=1 -> 1
+    //n=2 -> 1 + 4
+    //n=3 -> 1 + 4 + 8
+    //n=4 -> 1 + 4 + 8 + 12
+    //n=i == 1 + 4(1+2+...+i)
     public long coloredCells(int n) {
         return 2L*n*(n-1)+1;
     }
