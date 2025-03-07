e22750cf09d52a169cf5d050cfcee0ddc02a239c
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-03-07 16:15:06 +0900

    Time: 97 ms, Memory: 50.5 MB - LeetHub

---
+++ 0552-50-2523-closest-prime-numbers-in-range/0552-50-2523-closest-prime-numbers-in-range.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] closestPrimes(int left, int right) {
+        boolean[] primes = new boolean[right + 1];
+        Arrays.fill(primes, true);
+        primes[0] = primes[1] = false;
+
+        for (int i = 2; i * i <= right; i++) {
+            if (primes[i]) {
+                for (int j = i * i; j <= right; j += i) {
+                    primes[j] = false;
+                }
+            }
+        }
+
+        List<Integer> temp = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            if (primes[i]) {
+                temp.add(i);
+            }
+        }
+
+        int[] result = {-1, -1};
+        if (temp.size() < 2) {
+            return result;
+        }
+
+        int min  = Integer.MAX_VALUE;
+         for (int i = 1; i < temp.size(); i++) {
+            int cur = temp.get(i) - temp.get(i - 1);
+            if (cur < min) {
+                min = cur;
+                result[0] = temp.get(i - 1);
+                result[1] = temp.get(i);
+            }
+        }
+        
+        return result;
+    }
+}
