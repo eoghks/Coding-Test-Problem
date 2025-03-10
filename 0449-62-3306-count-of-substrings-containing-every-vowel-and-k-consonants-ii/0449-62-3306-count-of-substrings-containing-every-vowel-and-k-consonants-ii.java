2dc34dc6f1c433860429e0f6135b2832ad655744
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-03-10 19:17:09 +0900

    Time: 282 ms, Memory: 46.1 MB - LeetHub

---
+++ 0449-62-3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/0449-62-3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public long countOfSubstrings(String word, int k) {
+        return solution(word, k) - solution(word, k + 1);
+    }
+     private long solution(String word, int k) {
+        int n = word.length();
+        long res = 0;
+        int consonants = 0;
+        int left = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int right = 0; right < n; right++) {
+            char c = word.charAt(right);
+            if (isVowel(c)) {
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            } else {
+                consonants++;
+            }
+            //5개의 모음이 존재하고 자음이 k개 이상인 경우
+            while (map.size() == 5 && consonants >= k) {
+                res += n - right;
+                char leftChar = word.charAt(left);
+                if (isVowel(leftChar)) {
+                    map.put(leftChar, map.get(leftChar) - 1);
+                    if (map.get(leftChar) == 0) {
+                        map.remove(leftChar);
+                    }
+                } else {
+                    consonants--;
+                }
+                left++;
+            }
+        }
+        return res;
+    }
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}
