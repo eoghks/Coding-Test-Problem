c8929b64710402574d6ad9d979305334239f207e
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-03-14 15:54:35 +0900

    Time: 32 ms, Memory: 63.4 MB - LeetHub

--- 1373-53-2226-maximum-candies-allocated-to-k-children/1373-53-2226-maximum-candies-allocated-to-k-children.java
+++ 1373-53-2226-maximum-candies-allocated-to-k-children/1373-53-2226-maximum-candies-allocated-to-k-children.java
@@ -2,10 +2,10 @@ class Solution {
     public int maximumCandies(int[] candies, long k) {
         int res = 0;
         int min =1;
-        int right = 10000000;
+        int max = 10000000;
 
-        while(min <= right) {
-            int mid = min + (right - min) / 2;
+        while(min <= max) {
+            int mid = min + (max - min) / 2;
             long cnt = 0;
 
             for (int c : candies) {
@@ -16,7 +16,7 @@ class Solution {
                 res = mid;
                 min = mid + 1;
             } else {
-                right = mid - 1;
+                max = mid - 1;
             }
         }
         return res;
