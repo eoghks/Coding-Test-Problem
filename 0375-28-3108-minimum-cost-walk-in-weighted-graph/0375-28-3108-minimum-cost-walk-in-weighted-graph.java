422b12a00bf1746598948a9e855774171ed29f94
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-03-20 17:12:48 +0900

    Time: 7 ms, Memory: 125.5 MB - LeetHub

---
+++ 0375-28-3108-minimum-cost-walk-in-weighted-graph/0375-28-3108-minimum-cost-walk-in-weighted-graph.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] minimumCost(int n, int[][] edges, int[][] query) {
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        
+        int[] minPath = new int[n];
+        Arrays.fill(minPath, -1);
+        for (int[] edge : edges) {
+            int s = edge[0];
+            int t = edge[1];
+            int w = edge[2];
+            int sRoot = findRoot(parent, s);
+            int tRoot = findRoot(parent, t);
+
+            minPath[tRoot] &= w;
+
+            if (sRoot != tRoot) {
+                minPath[tRoot] &= minPath[sRoot];
+                parent[sRoot] = tRoot;
+            }
+        }
+
+        int[] res = new int[query.length];
+        for (int i = 0; i < query.length; i++) {
+            int s = query[i][0];
+            int t = query[i][1];
+            if (s == t) {
+                res[i] = 0;
+            } else if (findRoot(parent, s) != findRoot(parent, t)) {
+                res[i] = -1;
+            } else {
+                res[i] = minPath[findRoot(parent, s)];
+            }
+        }
+        return res;
+    }
+
+    private int findRoot(int[] parent, int node) {
+        if(parent[node] != node) {
+            parent[node] = findRoot(parent, parent[node]);
+        }
+        return parent[node];
+    }
+}
