6bc59cd5bcf3d0e134e2521b0941101f739413f3
DAEHAWN WE <62344247+eoghks@users.noreply.github.com>
2025-04-01 23:41:05 +0900

    Time: 5 ms, Memory: 107.9 MB - LeetHub

--- 2658-77-2140-solving-questions-with-brainpower/2658-77-2140-solving-questions-with-brainpower.java
+++ 2658-77-2140-solving-questions-with-brainpower/2658-77-2140-solving-questions-with-brainpower.java
@@ -1,11 +1,11 @@
 class Solution {
-    //brainpower가 n을 넘어가는 경우에는 현재 point만 획득(다음 문제를 풀수 없기 때문)
     public long mostPoints(int[][] questions) {
         int n = questions.length;
         long[] dp = new long[n];
         dp[n-1] = questions[n-1][0];
         for(int i=n-2; i>=0; i--) {
             int idx = i + questions[i][1] + 1;
+            //brainpower가 n을 넘어가는 경우에는 현재 point만 획득(다음 문제를 풀수 없기 때문)
             if(idx >= n) {
                 dp[i] = questions[i][0];
             } else {
