49feba16c96bd5137086b2675816f03b2620877f
DAEHAWN WE <zhfldk7316@naver.com>
2025-04-16 17:52:31 +0900

    Time: 0 ms (100%), Space: 41.5 MB (89.53%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int[] dp = new int[n];
@@ -12,6 +13,27 @@ class Solution {
                     int prev = j>=2? dp[j-2]:0;
                     dp[i] = Math.max(dp[i], prev + prices[i] - prices[j]);
                 }
+            }
+        }
+        return dp[n-1];
+    }*/
+    //dp[j]+prices[i]-prices[j] => dp[j]-prices[j] ==> max ==> max+prices[i]
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[] dp = new int[n];
+
+        int max = Integer.MIN_VALUE; // dp[j - 2] - prices[j] ==> 가장 돈을 많이 벌고 2일 뒤 주식을 산 경우
+        for(int i=0; i<n; i++) {
+            if(i < 2) {
+                max = Math.max(max, -prices[i]);
+            }
+            if(i == 0) {
+                dp[0] = 0;
+            } else if(i==1) {
+                dp[1] = Math.max(prices[1] - prices[0], 0);
+            } else {
+                dp[i] = Math.max(dp[i - 1], max + prices[i]);
+                max = Math.max(max, dp[i - 2] - prices[i]);
             }
         }
         return dp[n-1];
