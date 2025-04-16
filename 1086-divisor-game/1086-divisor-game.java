f208a6202ff45a6b99f79bd9208fc84d97151f7a
DAEHAWN WE <zhfldk7316@naver.com>
2025-04-16 17:28:43 +0900

    Time: 0 ms (100%), Space: 40.2 MB (91.71%) - LeetHub

---
+++ 1086-divisor-game/1086-divisor-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n==1)
+            return false;
+        if(n==2)
+            return true;
+        for(int i=1; i<n; i++) {
+            if(n%i==0)
+                return !(divisorGame(n-i));
+        }
+        return false;
+    }
+}
