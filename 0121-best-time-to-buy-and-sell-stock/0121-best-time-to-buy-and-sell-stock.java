e5befbe654244c0ca49ff4c6bd141f456913911b
DAEHAWN WE <zhfldk7316@naver.com>
2025-04-24 11:44:47 +0900

    Time: 3 ms (7.04%), Space: 56.6 MB (99.78%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,30 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        //DP Practice
-        /*
         int n = prices.length;
-        int result = 0;
-        if(n < 2)
-            return result;
-
         int[] dp = new int[n];
-        for(int i = n - 1; i >= 1; i--) {
-           if(dp[i] > 0)
-               dp[i-1] = prices[i] - prices[i-1] + dp[i];
-            else
-                dp[i-1] =  prices[i] - prices[i-1];
-            
-            result = Math.max(dp[i-1], result);
+        int cur = -prices[0];
+        for(int i=1; i<n; i++) {
+            dp[i] = Math.max(dp[i-1], cur+prices[i]);
+            cur = Math.max(cur, -prices[i]);
         }
-        */
-        // real soultion
-        int min = prices[0];
-        int result = 0;
-        for(int i = 1; i < prices.length; i++) {
-            if(min > prices[i])
-                min = prices[i];
-            result = Math.max(result, prices[i] - min);
-        }
-        return result;
+        return dp[n-1];
     }
 }
