2a57fc47656f2edf241a3c7b28928518a33e7d5c
DAEHAWN WE <zhfldk7316@naver.com>
2025-05-20 16:53:33 +0900

    Time: 2 ms (2.68%), Space: 42.2 MB (32.5%) - LeetHub

---
+++ 3321-type-of-triangle/3321-type-of-triangle.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        Arrays.sort(nums);
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums){
+            set.add(num);
+        }
+
+        if(nums[0] + nums[1] <= nums[2]){
+            return "none";
+        } else if (set.size() == 1){
+            return "equilateral";
+        } else if(set.size() == 2) {
+            return "isosceles";
+        } 
+        return "scalene";
+    }
+}
