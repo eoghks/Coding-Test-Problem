d7ca214ea68cfd2b0456ff01c711a071eacfe25f
DAEHAWN WE <zhfldk7316@naver.com>
2025-05-21 18:02:48 +0900

    Time: 0 ms (100%), Space: 45.9 MB (16.27%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        for(int i = 0; i < m; i++){
+            if(row[i]) {
+                for(int j = 0; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for(int j = 0; j < n; j++) {
+            if(col[j]) {
+                for(int i = 0; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
